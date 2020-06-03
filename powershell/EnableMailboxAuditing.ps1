721d30f1a5577462e87b7287f8e357462effab2e
Jared Harrington-Gibbs <jhgworking@gmail.com>
2020-06-03 11:38:13 -0400

    Update EnableMailboxAuditing.ps1
    Refactor code to enable the use of accounts with MFA and to provide a progress bar.

--- powershell/EnableMailboxAuditing.ps1
+++ powershell/EnableMailboxAuditing.ps1
@@ -1,17 +1,30 @@
 #This script will enable non-owner mailbox access auditing on every mailbox in your tenancy
-#First, let's get us a cred!
-$userCredential = Get-Credential
 
 #This gets us connected to an Exchange remote powershell service
-$ExoSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $userCredential -Authentication Basic -AllowRedirection
-Import-PSSession $ExoSession -Name Get-Mailbox, Set-Mailbox
+import-module ExchangeOnlineManagement
+Connect-ExchangeOnline
+
+#Get the mailboxes!!!!
+Write-Output "Getting a list of mailboxes in the tenant." `
+"Please be patient this can take a while."
+$mailboxList = Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox" -or RecipientTypeDetails -eq "SharedMailbox" -or RecipientTypeDetails -eq "RoomMailbox" -or RecipientTypeDetails -eq "DiscoveryMailbox"}
+
+
 
 #Enable global audit logging
-foreach ($mailbox in Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox" -or RecipientTypeDetails -eq "SharedMailbox" -or RecipientTypeDetails -eq "RoomMailbox" -or RecipientTypeDetails -eq "DiscoveryMailbox"})
+$itemNumber = 0
+foreach ($mailbox in $mailboxList)
 {
     try
     {
+        Write-Progress -Activity "Updating $($mailbox.WindowsEmailAddress)" `
+                       -Status "User $($itemnumber) of $($mailboxList.count)" `
+                       -PercentComplete (($itemnumber*100)/$mailboxList.Count)
+        
+        $itemNumber++
+
         Set-Mailbox -Identity $mailbox.DistinguishedName -AuditEnabled $true -AuditLogAgeLimit 180 -AuditAdmin Update, MoveToDeletedItems, SoftDelete, HardDelete, SendAs, SendOnBehalf, Create, UpdateFolderPermission -AuditDelegate Update, SoftDelete, HardDelete, SendAs, Create, UpdateFolderPermissions, MoveToDeletedItems, SendOnBehalf -AuditOwner UpdateFolderPermission, MailboxLogin, Create, SoftDelete, HardDelete, Update, MoveToDeletedItems
+        
     }
     catch
     {
