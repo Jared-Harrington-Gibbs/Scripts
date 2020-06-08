a57b142125000a0786ad3a7375636107015e8ea0
Jared Harrington-Gibbs <jhgworking@gmail.com>
2020-06-08 11:07:26 -0400

    Update ConnectToSharePoint.ps1

--- powershell/O365Stuff/ConnectToSharePoint.ps1
+++ powershell/O365Stuff/ConnectToSharePoint.ps1
@@ -1,3 +1,7 @@
 #Because I keep forgetting how to connect to sharepoint
+
+$orgName=(Get-OrganizationConfig |select -ExpandProperty name).split(".")[0]
+#or
 $orgName="OrgNameHere"
+
 Connect-SPOService -Url https://$orgName-admin.sharepoint.com
