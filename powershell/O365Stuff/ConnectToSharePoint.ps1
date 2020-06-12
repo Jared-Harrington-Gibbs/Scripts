20a811793b7bc2809087ac54fa7df46b995d4f27
Jared Harrington-Gibbs <jhgworking@gmail.com>
2020-06-11 21:19:43 -0400

    Update ConnectToSharePoint.ps1

--- powershell/O365Stuff/ConnectToSharePoint.ps1
+++ powershell/O365Stuff/ConnectToSharePoint.ps1
@@ -1,7 +1,17 @@
 #Because I keep forgetting how to connect to sharepoint
 
-$orgName=(Get-OrganizationConfig |select -ExpandProperty name).split(".")[0]
-#or
 $orgName="OrgNameHere"
 
+#Parameters
+$TenantURL = "https://$orgName.sharepoint.com"
+ 
+#Tenant Admin URL
+$TenantAdminURL = "https://$orgName-admin.sharepoint.com"
+
+#PNP connect to Admin Center
+Connect-PnPOnline -Url $TenantAdminURL -UseWebLogin
+#or
+Connect-PnPOnline -Url $TenantAdminURL -SPOManagementShell
+
+#SPO connect to Admin Center
 Connect-SPOService -Url https://$orgName-admin.sharepoint.com
