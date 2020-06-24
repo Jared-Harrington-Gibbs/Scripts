60313482522bd2011295736455a034003ac301bb
Jared Harrington-Gibbs <jared@digitalgibbs.com>
2020-06-24 12:41:50 -0400

    Update update module manpages.ps1
    Add comment explaining this is only for use when update-help didn't update some modules by default.
    This shouldn't happen often but does for me sometimes.

--- powershell/update module manpages.ps1
+++ powershell/update module manpages.ps1
@@ -1,2 +1,3 @@
+#This manually specifies to update modules if update-help isn't getting everything.
 $modules = (Get-InstalledModule).Name
 $modules | ForEach-Object {Update-Help -Module $_ -Force -ErrorAction SilentlyContinue}
